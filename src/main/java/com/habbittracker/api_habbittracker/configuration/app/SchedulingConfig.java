eeb596c883779683abb82919a23bc698d39d95df
jose <jose@gmail.com>
2025-04-03 20:43:40 -0600

    Se agrego una clase de configuracion para correr tareas

---
+++ src/main/java/com/habbittracker/api_habbittracker/configuration/app/SchedulingConfig.java
@@ -0,0 +1,21 @@
+package com.habbittracker.api_habbittracker.configuration.app;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulingConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("scheduled-task-");
+        scheduler.setAwaitTerminationSeconds(60);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+}
